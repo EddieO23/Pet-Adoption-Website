b7fe5ba3f3fb031c6c162f23df9d1114e4fc8c84
Eddie <eocampo52@gmail.com>
2024-04-23 18:49:22 -0500

    finished dynamic weather feature

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ async function start() {
     const weatherPromise = await fetch("https://api.weather.gov/gridpoints/LOT/73,70/forecast")
     const weatherData = await weatherPromise.json()
     const ourTemperature = weatherData.properties.periods[0].temperature
-    console.log(ourTemperature)
+
+    document.querySelector("#temperature-output").textContent = ourTemperature
 }
 
 start()
