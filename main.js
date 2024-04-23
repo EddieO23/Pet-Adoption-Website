2b57143e6d7d74d2a14532ef0ce2aedb9d965099
Eddie <eocampo52@gmail.com>
2024-04-23 18:43:01 -0500

    added weather api integration into app

---
+++ main.js
@@ -0,0 +1,8 @@
+async function start() {
+    const weatherPromise = await fetch("https://api.weather.gov/gridpoints/LOT/73,70/forecast")
+    const weatherData = await weatherPromise.json()
+    const ourTemperature = weatherData.properties.periods[0].temperature
+    console.log(ourTemperature)
+}
+
+start()
