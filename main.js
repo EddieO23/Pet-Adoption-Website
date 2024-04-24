f6001a4f310356d009d31d21654c8d50434c05e0
eddie <eocampo52@gmail.com>
2024-04-23 20:11:31 -0500

    work in progress, fetched pet data

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// DYNAMIC WEATHER FEATURE
 async function start() {
     const weatherPromise = await fetch("https://api.weather.gov/gridpoints/LOT/73,70/forecast")
     const weatherData = await weatherPromise.json()
@@ -5,5 +6,15 @@ async function start() {
 
     document.querySelector("#temperature-output").textContent = ourTemperature
 }
-
 start()
+
+// DYNAMIC PETS AREA
+async function petsArea() {
+    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+    const petsData = await petsPromise.json()
+    petsData.forEach(pet => {
+        console.log(pet.species)
+    })
+}
+
+petsArea()
