72dc8f16d32a1b28a5623c5e6fe8eda804e42b7e
eddie <eocampo52@gmail.com>
2024-04-24 07:24:24 -0500

    added active styling to current filter button

--- main.js
+++ main.js
@@ -42,3 +42,21 @@ function createAgeText(birthYear) {
 
     return `${age} years old`
 }
+
+// PET FILTER BUTTON CODE
+const allButtons = document.querySelectorAll(".pet-filter button")
+
+allButtons.forEach(el => {
+    el.addEventListener('click', handleButtonClick)
+})
+
+function handleButtonClick(e) {
+    // remove active class from any and all buttons
+    allButtons.forEach(el => el.classList.remove('active'))
+
+    // add active class to the specific button that just got clicked
+    e.target.classList.add('active')
+    
+    // actually filter the pets down below
+
+}
